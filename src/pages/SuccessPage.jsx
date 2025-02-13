43ddb209a9c4d8a7021c8d5ad996e1de3ad16703
Soedjamal <akmalihksan181@gmail.com>
2025-02-13 11:59:55 +0700

    fix: ui success

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -4,7 +4,11 @@ import { Typography } from '@mui/material'
 const SuccessPage = () => {
     return (
         <>
-            <Typography textAlign="center" marginTop="40px" variant='h5' fontWeight="700" color="rgb(100,100,100)">Berhasil memilih.👀</Typography>
+            <div className="container" style={{ display: "flex", marginTop: "50px", alignItems: "center", padding: "0 20px" }}>
+                <img style={{ width: "130px", height: "150px", }} src="https://ik.imagekit.io/nir38m3b3/undraw_check-boxes_ewf2%20(1).svg?updatedAt=1739420816582" alt="" />
+                <Typography textAlign="center" variant='h4' fontWeight="700" color="#006787">Berhasil memilih.👀</Typography>
+            </div>
+            <Typography fontWeight="500" color="rgb(130,130,130)" textAlign="center" >"Terimakasih telah memilih"</Typography>
         </>
     )
 }
